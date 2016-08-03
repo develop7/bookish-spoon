43c7e4aa343dab234b0b03ef9e61cbe8943cd49f
Andrei Dziahel <develop7@develop7.info>
2016-08-03 20:33:59 +0300

    Implements task creation

--- Gemfile
+++ Gemfile
@@ -6,6 +6,11 @@ ruby '2.3', engine: 'ruby', engine_version: '2.3'
 gem 'sinatra'
 gem 'sinatra-contrib'
 
+gem 'mongoid', '~> 5.1', '>= 5.1.3'
+gem 'mongoid-geospatial', '~> 5.0'
+
+gem 'aasm', '~> 4.11'
+
 group :development do
   gem 'rspec'
 end
--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,76 @@
+require 'mongoid'
+require 'mongoid/geospatial'
 require 'sinatra/base'
+require 'aasm'
+require 'json'
 
-class GeoTasksApp < Sinatra::Base
+require 'pp'
+
+class Task
+  include Mongoid::Document
+  include Mongoid::Geospatial
+
+  field :name, type: String
+  field :auth_token, type: String
+  field :location, type: Point, sphere: true
+
+  include AASM
+  field :aasm_state
+  aasm do
+    state :created, initial: true
+    state :assigned
+    state :delivered
+    event :assign do
+      transitions from: :created, to: :assigned
+    end
+    event :deliver do
+      transitions from: :assigned, to: :delivered
+    end
+  end
+end
+
+class GeoTasksApp < Sinatra::Application
+  configure do
+    Mongoid.load!(File.join(__dir__, '..', 'config', 'mongoid.yml'))
+
+    Task.create_indexes
+  end
+
+  USER_TYPES = %i(manager driver)
+
+  # @param [Symbol] type Authorization type, can be :manager or :driver
+  def authorize!(type)
+    raise OperationNotSupportedError.new('Unsupported authorization type') unless USER_TYPES.include?(type)
+
+    auth = env['HTTP_AUTHENTICATION']
+    pp env
+
+    halt 403, 'Use "Authentication:" header' unless auth
+
+    halt 403, 'User type mismatch' unless auth[0].downcase == type[0].to_s.downcase # manager's tokens should start with 'M' and drivers' with 'D' accordingly
+
+    auth
+  end
 
   get '/' do
     'Hullo zeeba neiba!'
+  end
+
+  post '/tasks' do
+    token = authorize!(:manager)
+
+    form = JSON.parse(request.body.read, symbolize_names: true)
+
+    t = Task.new(form.merge({auth_token: token}))
+
+    if t.valid?
+      t.save.to_json
+    else
+      pp t.errors, t
+    end
+  end
+
+  get '/tasks' do
+    # ze_coll.find({pickup_coord: {'$nearSphere' => {'$geometry' => {type: "Point", coordinates: [-73.93414657, 40.82302903]}, '$maxDistance' => 5 * 10_000}}})
   end
 end
